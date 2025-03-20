bddc0d35d5753cf1f600ba2b7626de1565a71d75
nikshi-16 <nikshithadevadiga76>
2025-03-20 15:22:55 +0530

    03-create-website-layout

---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,22 @@
+import { ShoppingCartIcon,  } from 'lucide-react'
+import Link from 'next/link'
+
+export default function Menu() {
+  return (
+    <div className='flex justify-end'>
+      <nav className='flex gap-3 w-full'>
+        <Link href='/signin' className='flex item-center header-button'>
+         Hello, Sign in
+        </Link>
+
+        <Link href='/cart' className='header-button'>
+          <div className='flext item-end'>
+            <ShoppingCartIcon className='h-8 w-8'/>
+            Cart
+          </div>
+          
+        </Link>
+      </nav>
+    </div>
+  )
+}
