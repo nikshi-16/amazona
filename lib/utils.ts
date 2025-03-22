0df3a8212a4b937ef686f8d3d219ad58fb6481f8
nikshi-16 <nikshithadevadiga76>
2025-03-22 12:40:02 +0530

    19-pay-order-by-paypal

--- lib/utils.ts
+++ lib/utils.ts
@@ -128,3 +128,6 @@ export const formatDateTime = (dateString: Date) => {
     timeOnly: formattedTime,
   }
 }
+export function formatId(id: string) {
+  return `..${id.substring(id.length - 6)}`
+}
