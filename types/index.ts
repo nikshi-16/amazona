2ac13668c9269057bac8a907befe6c2924f12d47
nikshi-16 <nikshithadevadiga76>
2025-03-22 10:40:43 +0530

    17-create-checkout-page

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,4 @@
-import { CartSchema, OrderItemSchema, ProductInputSchema, UserInputSchema, UserSignInSchema,UserSignUpSchema} from "@/lib/validator";
+import { CartSchema, OrderItemSchema, ProductInputSchema, ShippingAddressSchema, UserInputSchema, UserSignInSchema,UserSignUpSchema} from "@/lib/validator";
 import { z } from "zod";
 
 
@@ -20,7 +20,7 @@ export type Data = {
 }
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
+export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
+export type IUserSignUp = z.infer<typeof UserSignUpSchema>
 export type IUserInput = z.infer<typeof UserInputSchema>
 export type IUserSignIn = z.infer<typeof UserSignInSchema>
-
-export type IUserSignUp = z.infer<typeof UserSignUpSchema>
